16dfaceee413650608cce48350237dc4570e5a2d
Danverde <gre12014@byui.edu>
2017-11-17 16:54:57 -0700

    should work. Parsing XML as a string, not as a DOM

--- main.js
+++ main.js
@@ -5,18 +5,62 @@
 /* Sanitize 'rel="noopener noreferrer" from quizzes to allow access to quiz questions via Canvas API'*/
 
 /* Put dependencies here */
-
-
+const asyncLib = require('async');
+const cheerio = require('cheerio');
 
 module.exports = (course, stepCallback) => {
-    course.addModuleReport('quiz-rel-cleaner');
+    course.addModuleReport('quizRelCleaner');
 
-    
-    console.log(course.content);
-    
-    //course.success('quiz-rel-cleaner', 'quiz-rel-cleaner successfully ...');
 
-    // course.throwErr('quiz-rel-cleaner', e);
+    //console.log(course.content);
 
-    stepCallback(null, course);
+
+    function scanQuiz(quiz, i, finalCb) {
+        /* convert dom to string*/
+        var quizContents = quiz.dom.xml();
+        /* replace nasty string(s) */
+        quizContents.replace(/rel=("|')noopener\s*noreferrer\1/g, '');
+
+        //quiz = cheerio.load(quizContents, {decodeEntities: false});
+
+        course.content[i] = cheerio.load(quizContents, {
+            decodeEntities: false
+        });
+
+        finalCb(null);
+    }
+
+    function findQuizzes(file, i, finalCb) {
+        if (/quiz_d2l_\d*\.xml/.test(file.name)) {
+            // is a quiz. Keep searching
+            scanQuiz(file, i, finalCb);
+        } else {
+            finalCb(null);
+        }
+    }
+
+    asyncLib.eachOf(course.content, findQuizzes, (err) => {
+        if (err) {
+            course.throwErr('quizRelCleaner', err);
+            // console.error(err);
+        }
+        console.log(course.content);
+        stepCallback(null, course);
+    });
+
+    //course.success('quizRelCleaner', 'quizRelCleaner successfully ...');
+
+
+    //stepCallback(null, course);
 };
+
+
+/*
+$('mattext').get().forEach((ele) => {
+    //console.log(count, $(ele).text());
+    a = $(ele).text();
+    a = a.replace(/rel=("|')noopener\s*noreferrer\1/g, '');
+    
+    
+});
+*/
