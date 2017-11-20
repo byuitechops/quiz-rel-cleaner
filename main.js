2f9ed34413eae9c886d96851ef0dff03fe501672
Danverde <gre12014@byui.edu>
2017-11-20 14:19:28 -0700

    First working version

--- main.js
+++ main.js
@@ -11,56 +11,66 @@ const cheerio = require('cheerio');
 module.exports = (course, stepCallback) => {
     course.addModuleReport('quizRelCleaner');
 
-
-    //console.log(course.content);
-
-
+    /***************************************************
+     * Converts each quiz DOM to a string, removes 
+     * bad rel's, and saves the altered string as a DOM
+     ***************************************************/
     function scanQuiz(quiz, i, finalCb) {
-        /* convert dom to string*/
-        var quizContents = quiz.dom.xml();
-        /* replace nasty string(s) */
-        quizContents.replace(/rel=("|')noopener\s*noreferrer\1/g, '');
+        /* convert dom to string */
+        var quizContents = quiz.dom.xml(),
+            itemsFound = 0;
 
-        //quiz = cheerio.load(quizContents, {decodeEntities: false});
+        /* count the strings */
+        itemsFound = quizContents.match(/rel=("|')noopener\s*noreferrer\1/g).length;
 
-        course.content[i].dom = cheerio.load(quizContents, {
-            decodeEntities: false
-        });
-
-        finalCb(null);
-    }
-
-    function findQuizzes(file, i, finalCb) {
-        if (/quiz_d2l_\d*\.xml/.test(file.name)) {
-            // is a quiz. Keep searching
-            scanQuiz(file, i, finalCb);
+        /* success if no dirty rel's were found */
+        if (itemsFound <= 0) {
+            course.success('quizRelCleaner', `No rel's found in ${quiz.name}`);
+            finalCb(null);
         } else {
+
+            /* replace nasty string(s) */
+            quizContents = quizContents.replace(/rel=("|')noopener\s*noreferrer\1/g, '');
+
+            /* save changes to the course Obj*/
+            course.content[i].dom = cheerio.load(quizContents, {
+                decodeEntities: false
+            });
+
+            /* Our work here is done */
+            course.success('quizRelCleaner', `${itemsFound} rel's removed from ${quiz.name}`);
             finalCb(null);
         }
     }
 
+    /*********************************
+     * Determines is a file is a quiz? 
+     *********************************/
+    function findQuizzes(file, i, finalCb) {
+        /* is the file a quiz? */
+        if (/quiz_d2l_\d*\.xml/.test(file.name)) {
+            /* if yes, bathe it */
+            scanQuiz(file, i, finalCb);
+        } else {
+            /* these are not the droids you're looking for */
+            finalCb(null);
+        }
+    }
+
+    /* start here */
     asyncLib.eachOf(course.content, findQuizzes, (err) => {
         if (err) {
             course.throwErr('quizRelCleaner', err);
-            // console.error(err);
         }
-        //console.log(course.content);
+
+        /* for testing, check each quiz for bad rel's */
+        /*course.content.forEach((item)=>{
+           if (/quiz_d2l_\d*\.xml/.test(item.name)) {
+               console.log(item.name);
+               console.log(item.dom.xml());
+           }
+        });*/
+
         stepCallback(null, course);
     });
-
-    //course.success('quizRelCleaner', 'quizRelCleaner successfully ...');
-
-
-    //stepCallback(null, course);
 };
-
-
-/*
-$('mattext').get().forEach((ele) => {
-    //console.log(count, $(ele).text());
-    a = $(ele).text();
-    a = a.replace(/rel=("|')noopener\s*noreferrer\1/g, '');
-    
-    
-});
-*/
